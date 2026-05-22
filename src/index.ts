beda7ef918f779d2895570fe191a12db4dd5b31c
artartur <arturgfs@hotmail.com>
2026-05-22 04:52:28 -0300

    chore: deploy tests

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import { Database } from "./database/connection";
 import { authRouter } from "./controllers/auth.controller";
 import { tasksRouter } from "./controllers/tasks.controller";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -31,3 +32,26 @@ new Database()
     console.error("MongoDB connection failed:", err.message);
     process.exit(1);
   });
+
+console.log("Starting application...");
+console.log("PORT:", PORT);
+console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Startup error:");
+    console.error(error);
+
+    process.exit(1);
+  }
+}
+
+startServer();
